c2c63d52c4dd37dd59bd6468bf7d70f686ba6df2
knaseem <knaseem22@gmail.com>
2016-06-07 20:05:31 -0400

    Initial Commit

---
+++ autoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  autoLayout
+//
+//  Created by Khalid Naseem on 07/06/2016.
+//  Copyright © 2016 Khalid Naseem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
